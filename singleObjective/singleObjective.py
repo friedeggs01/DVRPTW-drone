9c7b20ade95647f7cfac2bc5d46bfb465ec3c51e
friedeggs01 <nhokngok.online14203@gmail.com>
2024-09-02 23:58:14 +0700

    add heuristic

--- singleObjective/singleObjective.py
+++ singleObjective/singleObjective.py
@@ -55,7 +55,6 @@ class SingleObjectivePopulation(Population):
         self.indivs = self.indivs[:self.pop_size]
         return self.indivs[0]
 
-
 def trainSingleObjective(data_path, processing_number, indi_list, network, request_list,
                 functions, terminal_decision,terminal_ordering, terminal_choosing, 
                 pop_size, max_gen, min_height, max_height, initialization_max_height,  
@@ -157,6 +156,87 @@ def run_SingleObjective(data_path, processing_num,
     # carbon_upper = 100000
     # reject_upper = 1000
     best, res_gen = trainSingleObjective(data_path, processing_num, indi_list, network, request_list,
+                functions, terminal_decision,terminal_ordering, terminal_choosing, 
+                pop_size, max_gen, min_height, max_height, initialization_max_height,  
+                num_of_tour_particips, tournament_prob,crossover_rate, mutation_rate,
+                crossover_operator_list, mutation_operator_list, calFitness,
+                alpha, duration, start_train, end_train, 
+                decision_tree, ordering_tree, choosing_tree, carbon_upper, reject_upper)
+    return  best.objectives, res_gen
+
+def trainHeuristic(data_path, processing_number, indi_list, network, request_list,
+                functions, terminal_decision,terminal_ordering, terminal_choosing, 
+                pop_size, max_gen, min_height, max_height, initialization_max_height,  
+                num_of_tour_particips, tournament_prob,crossover_rate, mutation_rate,
+                crossover_operator_list, mutation_operator_list, calFitness,
+                alpha, duration, start_system_time, end_system_time, 
+                decision_tree, ordering_tree, choosing_tree, carbon_upper, reject_upper):
+    
+    pop = SingleObjectivePopulation(pop_size, functions, terminal_decision, terminal_ordering, terminal_choosing, 
+                                    min_height, max_height, initialization_max_height, 
+                                    num_of_tour_particips, tournament_prob, crossover_rate, mutation_rate)
+
+    pop.pre_indi_gen(indi_list)
+    pool = multiprocessing.Pool(processes=processing_number)
+    arg = []
+
+    for indi in pop.indivs:
+        arg.append((indi, network, request_list, duration, start_system_time, end_system_time))
+    result = pool.starmap(calFitness, arg)
+
+    # for indi in pop.indivs:
+    #     indi.objectives[0], indi.objectives[1] = calFitness(indi, network, request_list, duration, start_system_time, end_system_time)
+    for indi, value in zip(pop.indivs, result):
+        indi.objectives[0], indi.objectives[1] = value
+        indi.cal_fitness_indi(alpha, carbon_upper, reject_upper)
+    
+    # for indi in pop.indivs:
+    #     print(indi.objectives)
+    
+    best_indi = pop.natural_selection()
+    print("Generation 0:", best_indi.objectives)
+    
+    # Save result
+    # file_name = f'result\\{data_path}'
+    # os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    
+    # with open(file_name, "a") as file:
+    #     file.write(f"Generation 0: {best_indi.objectives}\n")
+    res_gen = [best_indi.objectives]
+    pool.close()
+    return best_indi, res_gen
+
+def run_heuristic(data_path, processing_num, 
+                num_vehicle, truck_capacity, drone_capacity, drone_endurance,
+                indi_list,  
+                functions, terminal_decision, terminal_ordering, terminal_choosing, 
+                pop_size, max_gen,  min_height, max_height, initialization_max_height,  
+                num_of_tour_particips, tournament_prob, crossover_rate, mutation_rate,
+                crossover_operator_list, mutation_operator_list, calFitness, 
+                decision_tree, ordering_tree, choosing_tree, 
+                alpha, duration, start_train, end_train, end_test):
+    reader = Read_data()
+    request_list = reader.read_request(data_path)      
+    network = Network(request_list, num_vehicle, truck_capacity, drone_capacity, drone_endurance)
+
+    sum_max_dis = 0
+    for i in range (len(request_list)):
+        max_each_request = 0
+        for j in range(len(request_list)):
+            if i != j:
+                max_each_request = max(max_each_request, cal_distance(request_list[i], request_list[j]))
+        sum_max_dis += max_each_request
+    depo_max = 0
+    for i in range(len(request_list)):
+        depo_max = max(depo_max, cal_distance(None, request_list[i]))
+    sum_max_dis  = sum_max_dis + 2*depo_max*network.num_vehicle
+    carbon_upper = sum_max_dis*network.WAER
+    reject_upper = len(request_list)
+    print("Carbon upper: ", carbon_upper)
+    print("Reject upper: ", reject_upper)
+    # carbon_upper = 100000
+    # reject_upper = 1000
+    best, res_gen = trainHeuristic(data_path, processing_num, indi_list, network, request_list,
                 functions, terminal_decision,terminal_ordering, terminal_choosing, 
                 pop_size, max_gen, min_height, max_height, initialization_max_height,  
                 num_of_tour_particips, tournament_prob,crossover_rate, mutation_rate,
